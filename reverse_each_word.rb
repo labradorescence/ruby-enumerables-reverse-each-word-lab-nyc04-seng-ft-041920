74fd5f59048bf02257a3a7ae5a53fbc2bf8d04fa
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-04 23:01:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (original)
   original = original.split(" ")
   reversed_arr = []
-  original.each |each_word| { reversed_arr << each_word.reverse }
+  original.each {|each_word|  reversed_arr << each_word.reverse }
   return reversed_arr
 end
